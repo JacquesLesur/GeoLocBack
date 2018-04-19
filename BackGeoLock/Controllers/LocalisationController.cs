fce52b0c96da07e132bca1455632b785be874a6b
JacquesLesur <lesurjacques@gmail.com>
2018-04-19 13:51:55 +0200

    Expiration datas

--- BackGeoLock/Controllers/LocalisationController.cs
+++ BackGeoLock/Controllers/LocalisationController.cs
@@ -21,15 +21,10 @@ namespace BackGeoLock.Controllers
         [EnableCors(origins: "*", headers: "*", methods: "*")]
         public List<Localisation> Get(string longitude, string latitude)
         {
-            //getPlacesAround(longitude, latitude);
-
+            
             longitude = longitude.Replace(".", ",");
             latitude = latitude.Replace(".", ",");
-            //a remplacer post fini
-            //Localisation loc = new Localisation("Surface-Jacques", "Ranger0310", -1.5616382,47.2168111, DateTime.Now );
-            //Localisation loc2 = new Localisation("TitanXp", "Netoun", -1.564179, 47.233920, DateTime.Now);
-            //Redis.addLocalisation(loc);
-            //////////
+            
             List<Localisation> listLoc = Redis.GetLocalisations();
             
             List<Localisation> listLocProx = new List<Localisation>();
@@ -37,7 +32,6 @@ namespace BackGeoLock.Controllers
             double latitudeUser = Convert.ToDouble(latitude);
             foreach (Localisation loca in listLoc)
             {
-                
                 bool longOk = loca.position.lng > longitudeUser - 0.01  && loca.position.lng  < longitudeUser + 0.01;
                 bool latOk = loca.position.lat > latitudeUser - 0.01 && loca.position.lat < latitudeUser + 0.01;
                 if (longOk && latOk)
@@ -48,58 +42,13 @@ namespace BackGeoLock.Controllers
             return listLocProx;
         }
 
-        private void getPlacesAround(string longitude, string latitude)
-        {
-            String username = "test";
-            String password = "test";
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(" https://api.openstreetmap.org/api/0.6/map?bbox=47.242145,-1.556662,47.372145,-1.426662");
-            request.Method = "GET";
-            request.ContentType = "application/json";
-            String encoded = Convert.ToBase64String(System.Text.Encoding.GetEncoding("ISO-8859-1").GetBytes(username + ":" + password));
-            request.Headers.Add("Authorization", "Basic " + encoded);
-            try
-            {
-                WebResponse webResponse = request.GetResponse();
-                using (Stream webStream = webResponse.GetResponseStream())
-                {
-                    if (webStream != null)
-                    {
-                        using (StreamReader responseReader = new StreamReader(webStream))
-                        {
-                            string response = responseReader.ReadToEnd();
-                            Console.Out.WriteLine(response);
-                        }
-                    }
-                }
-            }
-            catch (Exception e)
-            {
-               
-            }
-        }
-
+       
         // GET: api/Localisation
         [HttpGet]
         [EnableCors(origins: "*", headers: "*", methods: "*")]
         public List<Localisation> Get()
         {
-            //a supprimer post fini
-            //List<Localisation> listLoc = new List<Localisation>();
-            //Localisation loc = new Localisation("Surface-Jacques", "Ranger0310", 47.2168111, -1.5616382, DateTime.Now);
-            //Localisation loc2 = new Localisation("TitanXp", "Netoun", 47.233920, -1.564179, DateTime.Now);
-            //Redis.addLocalisation(loc);
-            //List<Localisation> localisations = Redis.GetLocalisations();
-            //listLoc.Add(loc);
-            //listLoc.Add(loc2);
-            ///////
             return Redis.GetLocalisations();
-        }
-
-        // GET: api/Localisation/5
-        [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
-        {
-            return "value";
         }
         
         // POST: api/Localisation
@@ -117,28 +66,15 @@ namespace BackGeoLock.Controllers
                 else
                 {
                     Redis.addLocalisation(localisation);
-                   
+
                 }
             }
-            catch(ArgumentException e)
+            catch (ArgumentException e)
             {
                 throw new System.Web.Http.HttpResponseException(HttpStatusCode.NotAcceptable);
             }
-           
-
             
         }
-        
-        // PUT: api/Localisation/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-        
-        // DELETE: api/ApiWithActions/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+
     }
 }
--- BackGeoLock/Controllers/Redis.cs
+++ BackGeoLock/Controllers/Redis.cs
@@ -10,35 +10,18 @@ namespace BackGeoLock.Controllers
 {
     public static class Redis
     {
-        
+        private static readonly RedisClient redisClient = new RedisClient("163.172.144.237", 6379, "master123", 0);
         public static void addLocalisation(Localisation localisation)
-        {
-            string host = "163.172.144.237";
-            string elementKey = "master123";
-            using (RedisClient redisClient = new RedisClient(host, 6379,elementKey))
-            {
-                
+        {             
                 IRedisTypedClient<Localisation> localisationRedis = redisClient.As<Localisation>();
                 IRedisList<Localisation> listUser = localisationRedis.Lists[$"Localisation:{localisation.pseudo}"];
+                //expire list 5h
+                redisClient.Custom("EXPIRE", $"Localisation:{localisation.pseudo}", "18000");
                 listUser.Add(localisation);
-                //localisationRedis.Store(localisation);
-
-                //localisationRedis.de();
-
-                //localisationRedis.Store(localisation);
-                //localisationRedis.SetValue(localisation.pseudo,localisation);
-
-                //var testGet = localisationRedis.GetById(localisation.Id);
-
-            }
+              
         }
         public static List<Localisation> GetLocalisations()
         {
-            string host = "163.172.144.237";
-            string elementKey = "master123";
-            using (RedisClient redisClient = new RedisClient(host, 6379, elementKey, 0))
-            {
-
                 IRedisTypedClient<Localisation> localisationRedis = redisClient.As<Localisation>();
                 List<string> keys = redisClient.GetAllKeys();
                 List<Localisation> listLastLoc = new List<Localisation>();
@@ -48,9 +31,8 @@ namespace BackGeoLock.Controllers
                     listLastLoc.Add(lastLoc);
                     
                 }
-                
                 return listLastLoc;
-            }
+            
         }
 
     }
